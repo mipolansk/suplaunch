78f9ed458ad6cecfa19ee90f5cf47fb2f8bd13cb
mipolansk <michal@polanscy.com.pl>
2024-02-27 23:04:15 +0100

    Version 0.0.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "org.supla.launcher"
         minSdk = 26
         targetSdk = 33
-        versionCode = 2
-        versionName = "0.0.2"
+        versionCode = 3
+        versionName = "0.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
